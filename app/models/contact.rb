d6c24ea2c82fcdf4d519e638ecf8a699cd11b60f
syada13 <suresh23_himcs@rediffmail.com>
2017-09-13 13:57:29 +0530

    fixed contact model rSpevc failure by adding validate_presence_of contact fields in contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,5 @@ class Contact < ApplicationRecord
   has_many :phone_numbers, dependent: :destroy
   accepts_nested_attributes_for :addresses, :reject_if => :all_blank, allow_destroy: true
   accepts_nested_attributes_for :phone_numbers, :reject_if => :all_blank, allow_destroy: true
+  validates_presence_of :first_name, :last_name, :gender
 end
